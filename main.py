731f598471f4d0d6a27c00ee43ec556ddc518740
rafelt412 <rafelt444@gmail.com>
2024-12-16 01:37:08 -0400

    atualização

--- main.py
+++ main.py
@@ -5,10 +5,5 @@ def banco_comidas():
     comidas = [line.strip() for line in lines]
     df = pd.DataFrame(comidas, columns=["comida"])
     return df
-def criar_animal():
-    lista = []
-    for _ in range(0,5):
-        animal = input("Digite o nome de um animal: ")
-        lista.append(animal)
-    print(lista)
-criar_animal()
+def validar(animal):
+    lista = 
---
+++ teste.py
@@ -0,0 +1,2 @@
+def criar():
+    pass
